4703aa4de5f85d018b6ba1527195e9eb5519e683
Andrey Kashin <askasin@yandex.ru>
2021-12-06 15:56:32 +0700

    my first react

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Header from "../Header/Header";
 
-const App = () => {}
+const App = () => {
+    return (
+        <>
+            <Header title="Мой первый заоголовок"/>
+            <Header title="Мой второй заоголовок"/>
+            <Header title="Мой третий заоголовок"/>
+        </>
+    )
+};
 
-export  default App;
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-const Header = () =>{}
+const Header = (props) => {
+
+    return (
+        // <h1>{props.title}</h1>
+        <p>{props.title}</p>
+    )
+};
+// const Parag = (props) => {
+//
+//     return (
+//         // <h1>{props.title}</h1>
+//         <p>{props.title}</p>
+//     )
+// };
 
 
-export default  Header;
+export default Header;
